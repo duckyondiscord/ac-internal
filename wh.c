ffb535c3d704e43c294924c8e1e5fcd6ab9aef3f
ducky <duccywastaken@protonmail.com>
2026-07-13 14:11:44 +0300

    Remove now useless includes

--- wh.c
+++ wh.c
@@ -11,12 +11,10 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <dlfcn.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <stdint.h>
-#include <sys/mman.h>
 #include <assert.h>
 #include "primitives/defs.h"
 #include "primitives/hook.h"
